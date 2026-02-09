265b14f6db40064a86fe60000793e11a0c6c650f
diegorezm <dmourarezende@gmail.com>
2026-02-09 14:20:42 -0300

    layout now holds the toast component

--- src/components/layouts/app-layout.tsx
+++ src/components/layouts/app-layout.tsx
@@ -1,6 +1,11 @@
+import ToastContainer from "../toast/container";
 import "./app.css";
 import { ParentComponent } from "solid-js";
 
 export const AppLayout: ParentComponent = (props) => {
-  return <>{props?.children}</>;
+  return (
+    <>
+      {props?.children} <ToastContainer />
+    </>
+  );
 };
