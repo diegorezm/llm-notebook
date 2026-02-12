90c48d3055e6ede08fa1b5ac40a4df066ffd0927
diegorezm <dmourarezende@gmail.com>
2026-02-11 21:55:31 -0300

    the database has to thread safe so i can create background jobs

--- src-tauri/src/state.rs
+++ src-tauri/src/state.rs
@@ -1,9 +1,14 @@
 use std::sync::Arc;
 use tauri::async_runtime::Mutex;
 
-use crate::{ai::embeds::EmbedModel, db::db_manager::DBManager};
+use crate::{
+    ai::{embeds::EmbedModel, llama::Model},
+    db::db_manager::DBManager,
+};
 
+#[derive(Clone)]
 pub struct AppState {
-    pub db: DBManager,
+    pub db: Arc<DBManager>,
     pub embeddings_model: Arc<Mutex<EmbedModel>>,
+    pub chat_model: Arc<Mutex<Model>>,
 }
